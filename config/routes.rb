cdbca289c2bc8752b296ae3d0b5e686b3c7817d5
Your Name <you@example.com>
2018-10-24 14:18:18 +1100

    add root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
-get 'welcome/home', to: "welcome#index"
+# get 'welcome/home', to: "welcome#index"
 get 'about', to: "welcome#about"
+root "welcome#index"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
